0d3d1f87bd245715217d6c8651b9da49c42925a2
Otgonbyr <otgonbayar0503@gmail.com>
2023-10-31 12:29:35 +0800

    og

---
+++ to-do/todo.js
@@ -0,0 +1,18 @@
+let body = document.querySelector('body')
+let container = document.querySelector('.container')
+let modal = document.querySelector('.modal')
+let add = document.querySelectorAll('.add')
+let addbtn = document.querySelector('.addbtn')
+let addtask = document.querySelector('.addtask')
+
+function modalcard() {
+    modal.style.opacity = '1'
+}
+function none() {
+    modal.style.opacity = '0'
+}
+addbtn.onclick = none
+add[0].onclick = modalcard
+add[1].onclick = modalcard
+add[2].onclick = modalcard
+add[3].onclick = modalcard
