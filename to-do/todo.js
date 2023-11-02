ee19816cb3f269ac534faae1d8233934b7890992
Otgonbyr <otgonbayar0503@gmail.om>
2023-11-02 23:34:14 +0800

    ee

--- to-do/todo.js
+++ to-do/todo.js
@@ -4,16 +4,22 @@ let modal = document.querySelector('.modal')
 let add = document.querySelectorAll('.add')
 let addbtn = document.querySelector('.addbtn')
 let addtask = document.querySelector('.addtask')
+let bgopa = document.querySelector('.bgopacity')
 
 function modalcard() {
-    modal.style.opacity = '1'
+    modal.style.display = 'flex'
 }
 function none() {
-    modal.style.opacity = '0'
+    modal.style.display = 'none'
 }
-function bgblack() {
-    addtask.style.backgroundColor = 'black'
+function bg() {
+    bgopa.style.display = 'flex'
 }
+
+add[0].onclick = bg
+add[1].onclick = bg
+add[2].onclick = bg
+add[3].onclick = bg
 
 addbtn.onclick = none
 add[0].onclick = modalcard
