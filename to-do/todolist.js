c6417608fc1c5e35f67309e0122dba1086eb981d
Otgonbyr <otgonbayar0503@gmail.om>
2023-11-08 23:46:23 +0800

    count

--- to-do/todolist.js
+++ to-do/todolist.js
@@ -6,7 +6,7 @@ let addcard = document.querySelector('.addcard')
 let addtaskbtn = document.querySelector('.addtaskbtn')
 let addbtn = document.querySelector('.addbtn')
 let removebtn = document.querySelector('.remove')
-let countnum = document.querySelectorAll('.countnum')
+let countnum = document.querySelectorAll('.count')
 
 function popup(){
     modal.style.display = "block"
@@ -17,21 +17,9 @@ function none(){
 // function uid() {
 // return "id-" + Math.random();    
 // }
-// console.log(uid());
+
 // function randomnum(){
 //     return String(Math.random(1))
-// }
-// let remove = []
-// function removecard(){
-//     remove.forEach(() =>{
-//         console.log();
-//     })
-// }
-
-// function remove() {
-//     if (uid() !== removebtn ) {
-//         removebtn.remove()
-//     }
 // }
 
 let count = {
@@ -40,7 +28,7 @@ let count = {
     stuck: 0,
     done: 0,  
 }
-
+console.log(count);
 let data = []
 
 addbtn.onclick = none
@@ -52,7 +40,7 @@ function adddata() {
     let description = document.getElementById('description').value
     let status = document.getElementById('status').value
     let priority = document.getElementById('priority').value
-    data.push({ title, description, status, priority , id:uid()})
+    data.push({ title, description, status, priority})
     render(data)
 }
 function render(data) {
@@ -63,7 +51,7 @@ function render(data) {
     empty[3].innerHTML = ""
     data.map((el) => {
         const carddata = `
-        <div class="cardtext bg-white flex gap-12 justify-between" ${el.id}>
+        <div class="cardtext bg-white flex gap-12 justify-between">
         <div class="done icon flex justify-center align-center grey-hover margin-top-5">
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
@@ -77,7 +65,7 @@ function render(data) {
             <div class="category flex justify-center align-center grey-hover margin-top-5">${el.priority}</div>
         </div>
         <div>
-            <div class= "remove icon flex justify-center align-center margin-top-5 grey-hover" ${el.remove}>
+            <div class= "remove icon flex justify-center align-center margin-top-5 grey-hover">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
                     <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"></path>
@@ -91,13 +79,17 @@ function render(data) {
         </div>`
         if(el.status === "todo"){
             empty[0].innerHTML += carddata
+            countnum[0].innerHTML += count.todo += 1
             // count.todo += 1
         }else if(el.status === "inprogress"){
             empty[1].innerHTML += carddata
+            // count.inprogress += 1
         }else if(el.status === "Stuck"){
             empty[2].innerHTML += carddata
+            // count.stuck += 1
         }else if(el.status === "Done"){
             empty[3].innerHTML += carddata
+            // count.done += 1
         }
         empty.innerHTML += carddata
         })
