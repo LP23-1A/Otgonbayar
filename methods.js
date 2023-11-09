bd2957a2559d4d902117cc126349d0e546aeac49
Otgonbyr <otgonbayar0503@gmail.com>
2023-11-09 13:01:30 +0800

    aaaaaaaaaaaaaaa

---
+++ methods.js
@@ -0,0 +1,18 @@
+// array methods+
+// join = 
+// includes = 
+// indexOf = array iin index butsaana
+// pop = ariin element iig ustgaad ustgasan element ee butsaana
+// push = array iin ard shineer element nemeh
+// shift = urdaas n ustgaad ustgasan element ee butsaana
+// unshift = urdaas n nemne 
+// splice = 
+
+// object methods+
+// keys = objectiin key uudiig array bolgon butsaadag
+// values = objectiin value uudiig array bolgon butsaadag
+// entries = buten object iig array blgono
+// assign = 2 object iig niiluulne, davhardsan key tei object baivuul suuliin value aar avdag 
+// hasOwn = uuriin haisan key bga esehiig shalgana (boolean utga butsaana)
+
+
--- to-do/todolist.js
+++ to-do/todolist.js
@@ -5,8 +5,13 @@ let modal = document.querySelector('.modal')
 let addcard = document.querySelector('.addcard')
 let addtaskbtn = document.querySelector('.addtaskbtn')
 let addbtn = document.querySelector('.addbtn')
-let removebtn = document.querySelector('.remove')
+let removebtn = document.querySelectorAll('.remove')
 let countnum = document.querySelectorAll('.count')
+
+    let counttodo = document.querySelector('.counttodo')
+    let countprogress = document.querySelector('.countprogress')
+    let countstuck = document.querySelector('.countstuck')
+    let countdone = document.querySelector('.countdone')
 
 function popup(){
     modal.style.display = "block"
@@ -14,13 +19,24 @@ function popup(){
 function none(){
     modal.style.display = "none"
 }
+
+function randomNum() {
+    return String(Math.random(1));
+}  
+
 // function uid() {
 // return "id-" + Math.random();    
 // }
 
-// function randomnum(){
-//     return String(Math.random(1))
+// function removecard(element) {
+//     const deletecard = data.filter((item) => item.id !== element.parentElement.id)
+//     data = deletecard
+//     render(deletecard)
 // }
+
+// removebtn.forEach((element) => {
+//     removebtn.onclick = () => remove(element);
+//   });
 
 let count = {
     todo: 0,
@@ -28,7 +44,7 @@ let count = {
     stuck: 0,
     done: 0,  
 }
-console.log(count);
+
 let data = []
 
 addbtn.onclick = none
@@ -41,8 +57,22 @@ function adddata() {
     let status = document.getElementById('status').value
     let priority = document.getElementById('priority').value
     data.push({ title, description, status, priority})
+    data.id = randomNum()
     render(data)
+    counttodo.innerHTML = count.todo
+    countprogress.innerHTML = count.inprogress
+    countstuck.innerHTML = count.stuck
+    countdone.innerHTML = count.done
 }
+function deleteItem(element) {
+    const findId = element.id;
+    const deletecard = data.filter((el) => {
+      console.log(el.id, findId);
+      return el.id !== findId;
+    });
+    data = deletecard;
+    render(data);
+  }
 function render(data) {
     const empty = document.querySelectorAll('.empty')
     empty[0].innerHTML = ""
@@ -65,7 +95,7 @@ function render(data) {
             <div class="category flex justify-center align-center grey-hover margin-top-5">${el.priority}</div>
         </div>
         <div>
-            <div class= "remove icon flex justify-center align-center margin-top-5 grey-hover">
+            <div class= "remove icon flex justify-center align-center margin-top-5 grey-hover" id="${el.id}">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
                     <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"></path>
@@ -79,18 +109,23 @@ function render(data) {
         </div>`
         if(el.status === "todo"){
             empty[0].innerHTML += carddata
-            countnum[0].innerHTML += count.todo += 1
-            // count.todo += 1
+            count.todo += 1
         }else if(el.status === "inprogress"){
             empty[1].innerHTML += carddata
-            // count.inprogress += 1
+            count.inprogress += 1
         }else if(el.status === "Stuck"){
             empty[2].innerHTML += carddata
-            // count.stuck += 1
+            count.stuck += 1
         }else if(el.status === "Done"){
             empty[3].innerHTML += carddata
-            // count.done += 1
+            count.done += 1
         }
         empty.innerHTML += carddata
         })
+        let removebtn = document.querySelectorAll('.remove')
+        removebtn.forEach((element) => {
+        element.onclick = function () {
+        deleteItem(element);
+        };
+          });
 }
