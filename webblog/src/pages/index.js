b8d0124f21cf4c5f8aaadda8540415204b8324b5
Otgonbyr <otgonbayar0503@gmail.com>
2023-12-04 13:01:44 +0800

    trendin

--- webblog/src/pages/index.js
+++ webblog/src/pages/index.js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
+import Trending from "@/components/Trending";
 
 export default function Home() {
   return (
     <div className="body w-[100%]">
-      <div className="container flex justify-center">
+      <div className="container flex flex-col items-center">
         <Navbar/>
+        <Trending/>
       </div>
     </div>
   )
