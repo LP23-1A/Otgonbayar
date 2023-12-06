830c292473df53a5864de2df504e4bc042e464a3
Otgonbyr <otgonbayar0503@gmail.om>
2023-12-07 01:20:45 +0800

    api

--- webblog/src/components/AllBlogCard.jsx
+++ webblog/src/components/AllBlogCard.jsx
@@ -1,13 +1,27 @@
-const data = [
-    {
-        img: "https://s3-alpha-sig.figma.com/img/3abf/26dd/585632b9d05dcfd0daffacedd55842f5?Expires=1702857600&Signature=GtIcIcyrku03di6uhi0mfVSqHwIdIsUNoUQebv4ln3q84G9ZINAh~g63In7v1hCCoAUoAP2aTUyCsm0UHjL-VzZqeJwqljXEBvlG38XfWgPV0jOZS41GSMMySeIgQ-5a6beEdkq3-nVqA8DtYQ5tFNSBjsKIZZlJohL6yXlNmptCXdFuApfCK6nWzmbfiNgeW-g3FK0QgNJNPlJaJFhYBW7ic31MDvl416i18XVjJb3BD8ahSb1F7OqOffjqQOGVpbAL4r4vPSLE8tV4YbaMpt1sknJsMh0hxR4lDqQ0txf88uFFFPq7xf9FpZyaEIntEtG8EVMtjjmahArIWExyCw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-        category: "Technology",
-        title: "The Impact of Technology on the Workplace: How Technology is Changing",
-        date: "August 20, 2022",
-    },
-]
+import axios from "axios"
+import { useEffect, useState } from "react";
+let api = "https://dev.to/api/articles?username=gereltuyamz";
+
+// const data = [
+//     {
+//         img: "https://s3-alpha-sig.figma.com/img/3abf/26dd/585632b9d05dcfd0daffacedd55842f5?Expires=1702857600&Signature=GtIcIcyrku03di6uhi0mfVSqHwIdIsUNoUQebv4ln3q84G9ZINAh~g63In7v1hCCoAUoAP2aTUyCsm0UHjL-VzZqeJwqljXEBvlG38XfWgPV0jOZS41GSMMySeIgQ-5a6beEdkq3-nVqA8DtYQ5tFNSBjsKIZZlJohL6yXlNmptCXdFuApfCK6nWzmbfiNgeW-g3FK0QgNJNPlJaJFhYBW7ic31MDvl416i18XVjJb3BD8ahSb1F7OqOffjqQOGVpbAL4r4vPSLE8tV4YbaMpt1sknJsMh0hxR4lDqQ0txf88uFFFPq7xf9FpZyaEIntEtG8EVMtjjmahArIWExyCw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+//         category: "Technology",
+//         title: "The Impact of Technology on the Workplace: How Technology is Changing",
+//         date: "August 20, 2022",
+//     },
+// ]
 
 const AllBlog = () => {
+    const [data, setData] = useState([]);
+    const get = async() => {
+        let response = axios.get(api)
+        setData(response)
+        console.log(response);
+    }
+    useEffect(() =>{
+        get()
+    },[])
+
     return (
         <div className="w-[392px] h-[476px] rounded-xl border-2 p-[16px]">
             {
--- webblog/src/pages/index.js
+++ webblog/src/pages/index.js
@@ -3,6 +3,7 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Trending from "@/components/Trending";
 
+
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center gap-[100px]">
