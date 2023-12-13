939595549fdba69343e4dcea8d0e6ec45f2cb14e
Otgonbyr <otgonbayar0503@gmail.com>
2023-12-13 09:37:36 +0800

    singlepost

--- webblog/src/components/Carsoule.jsx
+++ webblog/src/components/Carsoule.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
+import { CarsouleData } from "@/components/CarsouleData";
 
-export default function Carsoule({ articles }) {
+export default function Carsoule() {
 
-    const [activeIndex, setActiveIndex] = useState()
+    const [activeIndex, setActiveIndex] = useState(0)
 
     const right = () => {
         if (activeIndex === 2) {
@@ -22,42 +23,23 @@ export default function Carsoule({ articles }) {
 
     return(
         <section className="w-[1240px] h-[900px]">
-            {/* {articles.length != 0 && (
-                <div className="w-full relative text-black rounded-lg">
-                <img
-                    src={articles[activeIndex].social_image}
-                    className="w-full min-h-[600px] rounded-lg"
-                />
-                    <div className="bg-white rounded-lg absolute left-0 bottom-0 p-10 max-w-[518px] m-3 border border-[#E8E8EA]">
-                        <Tag color={articles[activeIndex]?.flare_tag?.bg_color_hex}>
-                        {articles[activeIndex]?.flare_tag?.name?.toUpperCase()}
-                        </Tag>
-                        <h1 className="text-[36px] font-semibold">
-                        {articles[activeIndex].title}
-                        </h1>
-                        <p className="text-[#97989F]">
-                        {Date(articles[activeIndex].published_at)}
-                        </p>
-                    </div>
-                </div>
-            )} */}
             <div className="relative">
                 <div className="absolute">
-                    <img className="rounded-[12px]" src="https://s3-alpha-sig.figma.com/img/eb4f/aad2/4394e91108e011b0d07581596959713b?Expires=1702857600&Signature=TEJqgZebhD5ngGJk43qte2~qf859bguz1g9w0nc-t-7JwZKqGO9HkQ4f7UsByRTKRdqOFoXLq-GeNWVTxyF9yaI-X8jNnKzpHHPB0mwm5217XLBDvleUnawK8Q52jyuf37X~tBpedwk-oALlTZsspRn-XWzxdTRQOMwtXGLvj~Kv~0VS~Cxf7gUiI44x1oZGIcO9HQbL7oAr57JaTLqKz-5STAIIaCV4bEzUKUSd3x8o89LHrwfVdB1rnbaOm-NrwsFjsCGap2EeymdGc1fM9MLK7cYBadPJIs062VLD9cvqO4LZZ7kzDuD2FWVmWuhODVPANFGn0YJgP78jzOayAg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+                    <img className="w-[1240px] h-[900px] rounded-[12px]" src={CarsouleData[activeIndex].img} alt="" />
                 </div>
-                <div className="absolute top-[550px] left-[20px] w-[598px] h-[252px] bg-white rounded-[12px]">
+                <div className="absolute top-[600px] left-[20px] w-[598px] h-[252px] bg-white rounded-[12px]">
                     <div className="p-[40px] flex flex-col gap-[24px]">
                         <div className="flex flex-col gap-[16px]">
-                            <button className="w-[100px] py-[5px] rounded-[6px] bg-[#4B6BFB] text-[white] ">Technology</button>
-                            <p className="text-[36px]">Grid system for better Design User Interface</p>
+                            <button className="w-[100px] py-[5px] rounded-[6px] bg-[#4B6BFB] text-[white] ">{CarsouleData[activeIndex].button}</button>
+                            <p className="text-[36px]">{CarsouleData[activeIndex].title}</p>
                         </div>
                         <div>
-                            <p className="text-[#97989F]">August 20, 2022</p>
+                            <p className="text-[#97989F]">{CarsouleData[activeIndex].date}</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="w-[1240px] relative flex justify-end top-[850px]">
+            <div className="w-[1240px] relative flex justify-end top-[930px]">
                 <div className="w-[90px] flex gap-[9px]">
                     <button onClick={left} className="w-[40px] h-[40px] flex items-center justify-center border-2 rounded-[6px]">
                         <svg xmlns="http://www.w3.org/2000/svg" width="9" height="17" viewBox="0 0 9 17" fill="none">
---
+++ webblog/src/components/CarsouleData.jsx
@@ -0,0 +1,20 @@
+export const CarsouleData = [
+    {
+        img:"https://www.figma.com/file/UXeZt3KeSuj2lmOqng6jei/image/eb4faad24394e91108e011b0d07581596959713b?fuid=1285439702974197633",
+        button: "Technology",
+        title: "Grid system for better Design User Interface",
+        date: "August 20, 2022"
+    },
+    {
+        img:"https://ae01.alicdn.com/kf/HTB1YnvkQSzqK1RjSZPcq6zTepXaB/7x5FT-Snow-Winter-Pororo-Penguin-Custom-Photo-Studio-Props-Backdrops-Background-Vinyl.jpg_Q90.jpg_.webp",
+        button: "Fashion",
+        title: "Grid system for better Design User Interface",
+        date: "August 20, 2022"
+    },
+    {
+        img:"https://w0.peakpx.com/wallpaper/717/585/HD-wallpaper-dark-souls-fire-keeper-games.jpg",
+        button: "NextJs",
+        title: "Grid system for better Design User Interface",
+        date: "August 20, 2022"
+    },
+]
